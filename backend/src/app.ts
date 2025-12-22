3d914e63c9d1e01e140dfe19d447e488bafd3c8f
BonifasiusRaditya <radityahendrianto@gmail.com>
2025-12-22 11:16:35 +0700

    depfix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -22,9 +22,7 @@ app.use(cors({
     origin: [
         'http://localhost:5173', 
         'http://localhost:5174',
-        'http://localhost:3000',
-        'https://raimes-q7hx.vercel.app',
-        'https://raimes.vercel.app',
+        'https://rem-plum.vercel.app',
         process.env.FRONTEND_URL || 'http://localhost:5173'
     ],
     credentials: true,
